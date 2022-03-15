79ee01980e8f7cb67f02f26002ec49af1bad72fe
Patrick Booth <patrickbooth@Patricks-MacBook-Air.local>
2022-03-15 13:45:31 +0000

    graphics update

--- webapp-start.sh
+++ webapp-start.sh
@@ -4,5 +4,8 @@ sudo iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp -m tcp --sport 443 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT
+# always pull the latest WebApp code
+sudo git clone https://github.com/patrick-80074/python-webapp
 sudo systemctl start nginx.service
 sudo gunicorn --workers=2 --chdir /home/admin/python-webapp main:app
+
